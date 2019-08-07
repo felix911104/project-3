0bdf84d0163234b1ea0abcf391ae3ecb8b05617c
Felix <46941065+felix911104@users.noreply.github.com>
2019-08-07 12:42:29 -0700

    update login/signup

--- src/components/pages/SignUp/index.js
+++ src/components/pages/SignUp/index.js
@@ -1,30 +1,37 @@
 import React from "react";
 import "./style.css";
 
-function SignUp(){
-    
-  return(
+function SignUp() {
+  return (
     <div>
-    <div className="container">
-  <div className="main">
-    <div className="logo">
-      <h1>S</h1>
+      <div className="container">
+        <div className="main">
+          <div className="logo">
+            <h1>S</h1>
+          </div>
+          <form action="/">
+            <input
+              type="text"
+              placeholder="User Name"
+              autoComplete="off"
+              required
+            />{" "}
+            <i className="fa fa-user" />
+            <input
+              type="password"
+              placeholder="password"
+              autoComplete="off"
+              required
+            />{" "}
+            <i className="fa fa-lock" />
+            <input type="submit" defaultValue="Log In" />
+            <h4>
+              Already have an account?<a href="/login"> Go Login!</a>
+            </h4>
+          </form>
+        </div>
+      </div>
     </div>
-    <form action="/">
-      <input type="text" placeholder="User Name" autoComplete="off" required />	<i className="fa fa-user" />
-      <input type="password" placeholder="password" autoComplete="off" required />	<i className="fa fa-lock" />
-      <input type="submit" defaultValue="Log In" />
-      <h4>Already have an account?<a href="/login"> Go Login!</a></h4>
-    </form>
-  </div>
-</div>
-
-
-  
-  
-</div>
-
-
-    )
+  );
 }
 export default SignUp;
